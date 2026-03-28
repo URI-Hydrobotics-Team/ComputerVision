18f1eae4f935e26a9517a5372864d04f45e12b68
Junrong Liu <junliuwesterly@gmail.com>
2026-03-28 15:51:10 -0400

    test2

--- scripts/camera_feed/main.cpp
+++ scripts/camera_feed/main.cpp
@@ -29,10 +29,10 @@ int main() {
         This will get frames from camera (already in H.264) -> specify the height and width -> parse the H.264 stream -> put it in a queue to create a buffer
         -> encode H.264 data into RTP packets
     */
-    gst_rtsp_media_factory_set_launch(factory, "( videotestsrc pattern=ball ! video/h-264, width=1280, height=720 ! h264parse ! queue ! rtph264pay name=pay0 pt=96 )");
+    // gst_rtsp_media_factory_set_launch(factory, "( videotestsrc pattern=ball ! video/x-raw width=1280 height=720 ! h264parse ! queue ! rtph264pay name=pay0 pt=96 config-interval=-1 )");
 
     // Some other possible setting to use
-    // gst_rtsp_media_factory_set_launch(factory, "( v4l2src device=/dev/video0 ! video/x-raw, format=YUY2, width=1280, height=720 ! videoconvert ! x264enc ! h264parse ! queue ! rtph264pay name=pay0 pt=96 )");
+    gst_rtsp_media_factory_set_launch(factory, "( videotestsrc pattern=ball ! video/x-raw, format=YUY2, width=1280, height=720 ! videoconvert ! x264enc ! h264parse ! queue ! rtph264pay name=pay0 pt=96 )");
 
     mounts = gst_rtsp_server_get_mount_points(server);
     gst_rtsp_mount_points_add_factory(mounts, "/cam1", factory);
